8d922efd4fee8aca21a6bf589272e97646bceec8
robert.siwicki@googlemail.com <robert.siwicki@googlemail.com>
2022-01-18 23:50:29 +0000

    added missing annotations

--- src/main/java/io/rockdata/hellocd/HellocdApplication.java
+++ src/main/java/io/rockdata/hellocd/HellocdApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@RestController
 public class HellocdApplication {
 
 
